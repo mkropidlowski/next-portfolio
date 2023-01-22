4ede8ca0c5a900f905426a6d811db6cd2dc1829c
Michał Kropidłowski <michal@MacBook-Air-Micha.local>
2023-01-22 13:44:23 +0100

    SETUP COMMIT

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ["@svgr/webpack"],
+        });
+
+        return config;
+    },
+    images: {
+        unoptimized: true,
+        domains: ["images.unsplash.com"],
+    },
+};
