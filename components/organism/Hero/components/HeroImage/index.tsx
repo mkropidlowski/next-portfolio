f6ac6b9f1aef1dea40ac66433b08fb26585588b3
Michał Kropidłowski <michal@MacBook-Air-Micha.local>
2023-01-30 18:39:13 +0100

    change responsive pageLayout

--- components/organism/Hero/components/HeroImage/index.tsx
+++ components/organism/Hero/components/HeroImage/index.tsx
@@ -5,14 +5,7 @@ import Image from "next/legacy/image";
 
 const HeroImage: FC = () => (
     <div className={style.image}>
-        <Image
-            src={backgroundImg}
-            height={"800"}
-            alt="Background img"
-            layout="fixed"
-            objectFit="cover"
-            priority={true}
-        />
+        <Image src={backgroundImg} alt="Background img" layout="fill" objectFit="cover" priority={true} />
     </div>
 );
 export default HeroImage;
