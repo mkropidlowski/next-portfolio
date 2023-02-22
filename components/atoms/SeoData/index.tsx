1144654e0e7dbb232f7f30f0b54e67b57acc7993
Michał Kropidłowski <michal@oppo-a54-5g-1.home>
2023-02-22 12:09:28 +0100

    add seo

--- components/atoms/SeoData/index.tsx
+++ components/atoms/SeoData/index.tsx
@@ -9,6 +9,13 @@ interface Props {
 const SeoData: FC<Props> = ({ title, description }) => (
     <Head>
         <title>{title}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta
+            name="keywords"
+            content="www, website, github, linkedin, front-end, junior front-end, tworzenie stron www, projektowanie, post, blog, react, next.js, scss, mobile, webdesing, klient, atrakcyjne ceny"
+        />
+        <meta name="robots" content="index, follow" />
         <meta name="description" content={description} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
