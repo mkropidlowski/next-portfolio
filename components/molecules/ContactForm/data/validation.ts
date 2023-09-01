a4c4c55f81ae4a33d800ff0d306b2d38b8428114
mkropidlowski <mkropidlowsky@gmail.com>
2023-09-01 22:39:18 +0200

    fix: edit validation in form

--- components/molecules/ContactForm/data/validation.ts
+++ components/molecules/ContactForm/data/validation.ts
@@ -2,15 +2,28 @@ import * as yup from "yup";
 
 const schema = {
     max80Characters: "E-mail jest zbyt długi",
+    emailRegex:
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 };
 
 export const validationSchema = yup.object().shape({
-    name: yup.string().required("Pole imię i nazwisko jest wymagane."),
+    name: yup
+        .string()
+        .required("Pole imię i nazwisko jest wymagane.")
+        .min(10, "Minimalna liczba znaków - 10.")
+        .max(100, "Maksymalna liczba znaków - 100."),
     email: yup
         .string()
         .required("Pole email jest wymagane.")
         .email("Wprowadź poprawny adres e-mail.")
         .trim()
-        .max(80, schema.max80Characters),
-    message: yup.string().required("Pole wiadomość jest wymagane."),
+        .matches(schema.emailRegex, "E-mail jest niezgodny.")
+        .max(80, schema.max80Characters)
+        .min(5, "Minumum 5 znaków."),
+
+    message: yup
+        .string()
+        .required("Pole wiadomość jest wymagane.")
+        .min(10, "Minimum liczba znaków - 10.")
+        .max(500, "Maksymalna liczba znaków - 500."),
 });
