0333502ebbed935187b1efc8781ef43e043f6438
mkropidlowski <mkropidlowsky@gmail.com>
2023-09-04 19:03:44 +0200

    fix

--- config/envs.ts
+++ config/envs.ts
@@ -3,7 +3,7 @@ export const publicEnvs = {
     GMAIL_PASSWORD: process.env.NEXT_PUBLIC_AUTH_PASS as string,
     CYPRESS_BASE_URL: process.env.NEXT_CYPRESS_BASE_URL as string,
     RECAPTCHA_WEB_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_WEB_KEY as string,
-    RECAPTCHA_SECRET_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY,
+    RECAPTCHA_SECRET_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY as string,
 };
 
 export const isDevServer = process.env.NODE_ENV === "development";
