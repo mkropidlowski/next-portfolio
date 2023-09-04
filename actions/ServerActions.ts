63b6d139a6a830e4ebb88c34e525004f38282338
mkropidlowski <mkropidlowsky@gmail.com>
2023-09-04 18:52:46 +0200

    fix: add CORS

--- actions/ServerActions.ts
+++ actions/ServerActions.ts
@@ -1,14 +1,27 @@
 "use server";
-
 import axios from "axios";
 
 export async function verifyCaptcha(token: string | null) {
-    const res = await axios.post(
-        `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`
-    );
-    if (res.data.success) {
-        return "success!";
-    } else {
-        throw new Error("Failed Captcha");
+    try {
+        const config = {
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, POST",
+                "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+            },
+        };
+
+        const res = await axios.post(
+            `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`,
+            config
+        );
+
+        if (res.data.success) {
+            return "success!";
+        } else {
+            throw new Error("Failed Captcha");
+        }
+    } catch (error) {
+        throw new Error(error.message);
     }
 }
